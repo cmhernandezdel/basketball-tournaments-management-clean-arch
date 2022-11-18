915e531766329d1bf7ec25fa58cf73c5636b17be
cmhernandezdel <cmhernandezdel@gmail.com>
2022-11-19 00:34:25 +0100

    Modelado de jugadores y posiciones.

--- src/BasketballTournaments/BasketballTournaments.Domain/Players/Player.cs
+++ src/BasketballTournaments/BasketballTournaments.Domain/Players/Player.cs
@@ -1,6 +1,36 @@
-﻿namespace BasketballTournaments.Domain;
+﻿namespace BasketballTournaments.Domain.Players;
 
 public sealed class Player
 {
-    public DNI IdNumber { get; }
+    public SpanishId IdNumber { get; }
+
+    public string Name { get; }
+
+    public string Surname { get; }
+
+    public int HeightCentimeters { get; private set; }
+
+    public double WeightKilograms { get; private set; }
+
+    public Position Position { get; }
+
+    public Player(SpanishId idNumber, string name, string surname, int heightCentimeters, double weightKilograms, Position position)
+    {
+        IdNumber = idNumber;
+        Name = name;
+        Surname = surname;
+        HeightCentimeters = heightCentimeters;
+        WeightKilograms = weightKilograms;
+        Position = position;
+    }
+
+    public void SetHeight(int heightInCentimeters)
+    {
+        HeightCentimeters = heightInCentimeters;
+    }
+
+    public void SetWeight(double weightInKilograms)
+    {
+        WeightKilograms = weightInKilograms;
+    }
 }
---
+++ src/BasketballTournaments/BasketballTournaments.Domain/Players/Position.cs
@@ -0,0 +1,8 @@
+﻿namespace BasketballTournaments.Domain.Players;
+
+public enum Position
+{
+    Guard = 0,
+    Forward = 1,
+    Center = 2
+}
