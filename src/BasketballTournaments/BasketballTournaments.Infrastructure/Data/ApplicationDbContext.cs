68d64a02cad388d08066ba7d783601c923ae6a07
Carlos Manuel Hernández Delgado <cmhernandezdel@gmail.com>
2023-02-18 15:45:52 +0100

    Add ApplicationDbContext.

---
+++ src/BasketballTournaments/BasketballTournaments.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using BasketballTournaments.Domain.Players;
+using BasketballTournaments.Domain.Teams;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasketballTournaments.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Player> Players => Set<Player>();
+
+    public DbSet<Team> Teams => Set<Team>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Apply configurations here
+    }
+
+}
