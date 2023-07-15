2ce465f9afe0b326830b3d296197c1d6766f6d82
Carlos Manuel Hernández Delgado <cmhernandezdel@gmail.com>
2023-07-16 01:06:58 +0200

    Tournament validation.

---
+++ src/BasketballTournaments/BasketballTournaments.Domain/Tournaments/Tournament.Constraints.cs
@@ -0,0 +1,6 @@
+namespace BasketballTournaments.Domain.Tournaments;
+
+public sealed partial class Tournament
+{
+    public const int MaxLengthVenue = 128;
+}
--- src/BasketballTournaments/BasketballTournaments.Domain/Tournaments/Tournament.cs
+++ src/BasketballTournaments/BasketballTournaments.Domain/Tournaments/Tournament.cs
@@ -1,8 +1,9 @@
 using BasketballTournaments.SeedWork;
+using FluentResults;
 
 namespace BasketballTournaments.Domain.Tournaments;
 
-public sealed class Tournament : Entity
+public sealed partial class Tournament : Entity
 {
     public DateTime StartTimestampUtc { get; }
 
@@ -10,11 +11,28 @@ public sealed class Tournament : Entity
 
     public string Venue { get; }
 
-    public Tournament(DateTime startTimestampUtc, DateTime endTimestampUtc, string venue)
+    public Result<Tournament> Create(DateTime startTimestampUtc, DateTime endTimestampUtc, string venue)
+    {
+        string trimmedVenue = venue.Trim();
+
+        Result validationResult = Result.Merge(
+            Result.FailIf(startTimestampUtc >= endTimestampUtc, "Start date and time cannot be after end date and time."),
+            Result.FailIf(string.IsNullOrEmpty(trimmedVenue), "Venue cannot be empty."),
+            Result.FailIf(trimmedVenue.Length > MaxLengthVenue, $"Venue cannot exceed {MaxLengthVenue} characters.")
+        );
+
+        if (validationResult.IsFailed)
+        {
+            return validationResult;
+        }
+
+        return Result.Ok(new Tournament(startTimestampUtc, endTimestampUtc, trimmedVenue));
+    }
+
+    private Tournament(DateTime startTimestampUtc, DateTime endTimestampUtc, string venue)
     {
         StartTimestampUtc = startTimestampUtc;
         EndTimestampUtc = endTimestampUtc;
         Venue = venue;
     }
-
 }
