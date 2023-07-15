5d083888eece6234e24f2c2fcdf2694d0b6aee4f
Carlos Manuel Hernández Delgado <cmhernandezdel@gmail.com>
2023-07-16 01:53:39 +0200

    Modify infrastructure layer.

--- src/BasketballTournaments/BasketballTournaments.Infrastructure/Data/ApplicationDbContext.cs
+++ src/BasketballTournaments/BasketballTournaments.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using BasketballTournaments.Domain.Players;
 using BasketballTournaments.Domain.Shared.ValueObjects;
 using BasketballTournaments.Domain.Teams;
+using BasketballTournaments.Domain.TournamentGames;
+using BasketballTournaments.Domain.Tournaments;
 using BasketballTournaments.Infrastructure.Players.Configurations;
 using BasketballTournaments.Infrastructure.Shared.ValueConverters;
 using Microsoft.EntityFrameworkCore;
@@ -12,6 +14,10 @@ public class ApplicationDbContext : DbContext
     public DbSet<Player> Players => Set<Player>();
 
     public DbSet<Team> Teams => Set<Team>();
+
+    public DbSet<Tournament> Tournaments => Set<Tournament>();
+
+    public DbSet<TournamentGame> TournamentGames => Set<TournamentGame>();
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
