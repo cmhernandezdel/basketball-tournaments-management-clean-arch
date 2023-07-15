3c69847cb58d2e078d132d28284a38fbd56e9145
Carlos Manuel Hernández Delgado <cmhernandezdel@gmail.com>
2023-07-16 01:18:38 +0200

    Tournament games.

--- src/BasketballTournaments/BasketballTournaments.Domain/TournamentGames/TournamentGame.cs
+++ src/BasketballTournaments/BasketballTournaments.Domain/TournamentGames/TournamentGame.cs
@@ -1,5 +1,6 @@
 using BasketballTournaments.Domain.Players;
 using BasketballTournaments.Domain.Teams;
+using BasketballTournaments.Domain.Tournaments;
 using BasketballTournaments.SeedWork;
 using FluentResults;
 
@@ -19,19 +20,44 @@ public sealed class TournamentGame : Entity
 
     public Guid MostValuablePlayerId { get; }
 
+    public Guid TournamentId { get; }
+
+    public DateTime TimestampUtc { get; }
+
     public Result<TournamentGame> Create(
         Team homeTeam,
         Team awayTeam,
-        
-    )
-
-    public TournamentGame(Team homeTeam, Team awayTeam, uint homeTeamPoints, uint awayTeamPoints, Stage stage, Player mostValuablePlayer) : base()
+        uint homeTeamPoints,
+        uint awayTeamPoints,
+        Stage stage,
+        Player mostValuablePlayer,
+        Tournament tournament,
+        DateTime timestampUtc)
     {
-        HomeTeamId = homeTeam.Id;
-        AwayTeamId = awayTeam.Id;
+        Result validationResult = Result.Merge(
+            Result.FailIf(homeTeam == awayTeam, "Home team cannot be the same as away team."),
+            Result.FailIf(homeTeamPoints == awayTeamPoints, "A basketball game cannot end in a tie."),
+            Result.FailIf(mostValuablePlayer.TeamId != homeTeam.Id && mostValuablePlayer.TeamId != awayTeam.Id, "The MVP is not on any of the two teams."),
+            Result.FailIf(timestampUtc > tournament.EndTimestampUtc || timestampUtc < tournament.StartTimestampUtc, "Game is outside tournament dates.")
+        );
+
+        if (validationResult.IsFailed)
+        {
+            return validationResult;
+        }
+
+        return Result.Ok(new TournamentGame(homeTeam.Id, awayTeam.Id, homeTeamPoints, awayTeamPoints, stage, mostValuablePlayer.Id, tournament.Id, timestampUtc));
+    }
+
+    private TournamentGame(Guid homeTeamId, Guid awayTeamId, uint homeTeamPoints, uint awayTeamPoints, Stage stage, Guid mostValuablePlayerId, Guid tournamentId, DateTime timestampUtc) : base()
+    {
+        HomeTeamId = homeTeamId;
+        AwayTeamId = awayTeamId;
         HomeTeamPoints = homeTeamPoints;
         AwayTeamPoints = awayTeamPoints;
         Stage = stage;
-        MostValuablePlayerId = mostValuablePlayer.Id;
+        MostValuablePlayerId = mostValuablePlayerId;
+        TournamentId = tournamentId;
+        TimestampUtc = timestampUtc;
     }
 }
